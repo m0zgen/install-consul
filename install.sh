cd69c3469106687c981c684504cd3630af62b837
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2023-10-25 18:34:55 +0600

    Basic steps

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Install consul to Debian distros
+# Created by Yevgeniy Goncharov, http://sys-adm.in
+
+# Sys env / paths / etc
+# -------------------------------------------------------------------------------------------\
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+# Installation
+# -------------------------------------------------------------------------------------------\
+
+# Install packages
+apt-get update && apt-get install gnupg2 curl lsb-release
+
+# Install consul's gpg key
+curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
+
+# Add hashicorp's repository to sources.list
+echo "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main" > /etc/apt/sources.list.d/hashicorp.list
+
+# Update and install consul
+apt-get update
+apt-get install consul
+
+# Final Checking
+# -------------------------------------------------------------------------------------------\
+# Check Consul installation version
+consul -v
